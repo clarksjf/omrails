4a8413efe6a55de9ddee7451385bb685cc85a761
clarksjf <sjf.clark@gmail.com>
2017-01-05 19:20:23 +0000

    fix to lack of images001

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,12 +87,15 @@ Rails.application.configure do
 
   # SC added in order to allow images to be saved to amazon s3
 config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
-  }
+    storage: :s3,
+    :s3_host_name => 's3-eu-central-1.amazonaws.com',
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
 }
+
+
 end
