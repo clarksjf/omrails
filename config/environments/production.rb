0cd3b4638027a8533af90c8e66148ab96024be73
clarksjf <sjf.clark@gmail.com>
2017-01-05 19:24:33 +0000

    images fix on heroku 002

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,6 +95,8 @@ config.paperclip_defaults = {
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
     }
+      :url =>':s3_domain_url',
+  :path => '/:class/:attachment/:id_partition/:style/:filename'
 }
 
 
