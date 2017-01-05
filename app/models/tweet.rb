caf1c47f66912959cb6aa8c88da55ef0969010f6
clarksjf <sjf.clark@gmail.com>
2017-01-05 19:51:39 +0000

    images fix 007

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,6 +5,6 @@ class Tweet < ApplicationRecord
   validates :content, presence: true, length: { maximum: 140 }
 
   has_attached_file :image 
-  :url =>':s3_domain_url'
+
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
