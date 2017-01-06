ebb44db3f1e2faa85470778904caf98648c2928b
clarksjf <sjf.clark@gmail.com>
2017-01-06 07:57:20 +0000

    images fix 007

--- config/initializers/paperclip.rb
+++
@@ -1,8 +0,0 @@
-# File added in order to try and get images loading up correctly. 
-# Whole file should be deleted if it causes an issue sc.
-# config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-
-# config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-central-1.amazonaws.com'
